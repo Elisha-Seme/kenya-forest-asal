7f877c7246eab82abd5a33e4db1f2fba0b6361da
Elisha Papa <elishaseme99@gmail.com>
2026-02-12 15:30:45 +0300

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/kenya-forest-asal',
+  images: {
+    unoptimized: true,
+  },
   reactCompiler: true,
 };
 
