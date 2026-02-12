90c9b5e4c3dab4e9edff833965a8823924d870fd
Elisha Papa <elishaseme99@gmail.com>
2026-02-12 16:09:24 +0300

    Update basePath for VPS deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/kenya-forest-asal' : '',
+  basePath: process.env.GITHUB_ACTIONS ? '/kenya-forest-asal' : '',
   images: {
     unoptimized: true,
   },
