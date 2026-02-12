8a649cd9dcbcd76595fcc5497b440f2ddc41b7da
Elisha Papa <elishaseme99@gmail.com>
2026-02-12 15:43:50 +0300

    Make basePath conditional for local development

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/kenya-forest-asal',
+  basePath: process.env.NODE_ENV === 'production' ? '/kenya-forest-asal' : '',
   images: {
     unoptimized: true,
   },
